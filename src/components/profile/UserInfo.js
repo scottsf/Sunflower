e444a65f5a460aebcb7849df42d4c71c2326d9f3
Munkhtegsh <munkhtegsh@outlook.com>
2019-06-11 03:27:13 -0700

    Profile page shows the date and time when user joined

--- src/components/profile/UserInfo.js
+++ src/components/profile/UserInfo.js
@@ -1,17 +1,26 @@
 import React, { Fragment } from "react";
-import Post from '../blog/Post'
+import Post from "../blog/Post";
+
+const formatDate = date => {
+    const newDate = new Date(date).toLocaleDateString('en-US')
+    const newTime = new Date(date).toLocaleTimeString('en-US')
+    return `${newDate} at ${newTime}`
+
+}
 
 const UserInfo = ({ session }) => (
   <Fragment>
     <p>Name: {session.me.name}</p>
     <p>Email: {session.me.email}</p>
-    <p>Join Date: {session.me.createdAt} </p>
-    {
-        session.me.likes.map(post => (
-            <Post key={post.id} {...post}/>
-        ))
-    }
-
+    <p>Join Date: {formatDate(session.me.createdAt)} </p>
+    {session.me.likes.map(post => (
+      <Post key={post.id} {...post} />
+    ))}
+    {session.me.likes.length === 0 && (
+      <p>
+        <strong>You have no favorite posts yet!</strong>
+      </p>
+    )}
   </Fragment>
 );
 
